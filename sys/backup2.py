f01da68de02fa9cbfa45c5fb78386d27f3abe07a
Денис Туляков <dtulyakov@gmail.com>
2014-11-06 10:03:55 +0300

    В ветке master  Изменения для закрепления: 	modified:   ../.gitignore 	modified:   backup.py 	new file:   backup2.py

--- sys/backup2.py
+++ sys/backup2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 import ConfigParser
 import os
 import time
@@ -6,20 +6,24 @@ import getpass
 
 def get_dump():
     print "Enter user:"
-    user = raw_input()
+    #user = raw_input()
+    user = "root"
 
     print "Password will not be visible:"
-    password = getpass.getpass()
+    #password = getpass.getpass()
+    password = "root"
 
     print "Enter host:"
-    host = raw_input()
+    #host = raw_input()
+    host = "10.0.3.200"
 
     print "Enter database name:"
-    database = raw_input()
+    #database = raw_input()
+    database = "mysql"
 
 
     filestamp = time.strftime('%Y-%m-%d-%I:%M')
-    os.popen("mysqldump -u %s -p%s -h %s -e --opt -c %s | gzip -c > %s.gz" % (user,password,host,database,database+"_"+filestamp))
+    os.popen("mysqldump -h%s -u%s -p%s -e --opt -c %s | gzip -c > %s.gz" % (user,password,host,database,database+"_"+filestamp))
 
     print "\n-- please have a the dump file in "+database+"_"+filestamp+".gz --"
 
